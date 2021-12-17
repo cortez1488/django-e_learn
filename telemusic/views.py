545f092dc3de2b7eb47b6c508114b9d04262b163
cortez1488 <mlm.ui0@mail.ru>
2021-12-17 22:47:51 +0500

    'download'

--- telemusic/views.py
+++ telemusic/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from .service import handle_uploaded_file
 from .forms import UploadFileForm
 from .models import Music
 # Create your views here.
 
 class Download(View):
-    def get(self, request):pass
+    def get(self, request):
+        response = FileResponse(open(request.GET['path'], 'rb'))
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(request.GET['name'])
+        return response
 
 class Upload(View):
     def post(self, request):
